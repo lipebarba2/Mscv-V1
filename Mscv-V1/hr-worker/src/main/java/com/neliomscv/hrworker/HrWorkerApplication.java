68ccc90137fb59109099e7914be94382622aed47
lipebarba2 <felipecinemkt@gmail.com>
2025-06-03 20:30:05 -0300

    Eureka e Feign

--- Mscv-V1/hr-worker/src/main/java/com/neliomscv/hrworker/HrWorkerApplication.java
+++ Mscv-V1/hr-worker/src/main/java/com/neliomscv/hrworker/HrWorkerApplication.java
@@ -3,8 +3,11 @@ package com.neliomscv.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HrWorkerApplication {
 
 	public static void main(String[] args) {
